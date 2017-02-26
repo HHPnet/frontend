2198a704a221fd014b9d6631b61fdec27147dfc9
Javi <webmasterhhp@gmail.com>
2017-02-26 21:01:33 +0100

    Removed material ui and added bootstrap

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Row from 'react-bootstrap/lib/Row'
+import Col from 'react-bootstrap/lib/Col'
 
 export default class Home extends React.Component {
     render() {
         return (
-            <div>Home</div>
+            <Row>
+                <Col xs={12}>
+                    Home
+                </Col>
+            </Row>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import App from './components/common/app/App'
+import 'bootstrap/dist/css/bootstrap.css'
+//import 'bootstrap/dist/css/bootstrap-theme.css'
+//import 'bootstrap-material-design/dist/css/bootstrap-material-design.css'
+//import 'bootstrap-material-design/dist/css/ripples.css'
 import './index.css'
 
 injectTapEventPlugin()
